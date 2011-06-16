149351b60ff3c5316e93572052a4a86ceb14b8e1
ggrothendieck <ggrothendieck@4583039d-9635-0410-a725-4f087ce16b47>
2011-06-16 23:41:27 +0000

    startup message if default is not sqlite

---
+++ trunk/R/zzz.R
@@ -0,0 +1,20 @@
+
+.First.lib <- function(libname, pkgname) {
+	drv <- getOption("sqldf.driver")
+	drv <- if (is.null(drv) || drv == "") {
+		if ("package:RpgSQL" %in% search()) { 
+			"pgSQL"
+		} else if ("package:RMySQL" %in% search()) { 
+			"MySQL" 
+		} else if ("package:RH2" %in% search()) { 
+			"H2" 
+		} else "SQLite"
+	} else if (!tolower(drv) %in% c("pgsql", "mysql", "h2")) {
+		"SQLite"
+	}
+	if (drv != "SQLite") {
+		msg <- paste("sqldf will default to using", drv)
+		packageStartupMessage(msg)
+	}
+}
+
